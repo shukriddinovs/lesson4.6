1f1ce75b9bf21deb5ca43dd7e7c59154646e1c21
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-30 15:43:04 +0500

    {.}

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 
-let number = prompt("  birinchi  son kiriting ") 
-let number1 = prompt(" ikkinchi  son kiriting")
-let amal = prompt("amalni talang " + "\n.1.qoshish " + "\n.2.ayirish " + "\n.3.kopaytirish " +"\n.4.bolish " + "\n.amalni talang: (1/2/3/4); ")
-if ( number  && + number1 ) {
-    console.log( number + number1   );
-}else{
-    
+let number1 = +prompt("  birinchi  son kiriting ") 
+let number2 = +prompt(" ikkinchi  son kiriting")
+let amal = +prompt("amalni talang " + "\n.1.qoshish " + "\n.2.ayirish " + "\n.3.kopaytirish " +"\n.4.bolish " + "\n.amalni talang: (1/2/3/4); ")
+let sum;
+if (amal === 1) {
+    sum = number1 + number2;
+    console.log(sum);
+}else  if(amal === 2){
+    sum = number1 - number2;
+    console.log(sum);
+}if (amal === 3) {
+    sum = number1 * number2;
+    console.log(sum);
+}else  if(amal === 4){
+    sum = number1 / number2;
+    console.log(sum);
 }
+
