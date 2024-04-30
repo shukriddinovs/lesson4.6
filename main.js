4c5f9c8883fdf2a07cebcc0eb78b28afe6cb0cbe
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-30 14:04:46 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let number = prompt("  birinchi  son kiriting ") 
+let number1 = prompt(" ikkinchi  son kiriting")
+let amal = prompt("amalni talang " + "\n.1.qoshish " + "\n.2.ayirish " + "\n.3.kopaytirish " +"\n.4.bolish " + "\n.amalni talang: (1/2/3/4); ")
+if ( number  && + number1 ) {
+    console.log( number + number1   );
+}else{
+    
+}
